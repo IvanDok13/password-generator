920b80c381779650ad6a2ea0e0a74c14a2648443
Ivan Klochkov <Klochkov.Ivan.Sergeevich@gmail.com>
2023-06-05 13:33:56 +0300

    feat: add checkbox styles

--- src/ui/checkbox/checkbox.js
+++ src/ui/checkbox/checkbox.js
@@ -1,8 +1,17 @@
 import React from 'react';
-// import cx from 'clsx';
+import cx from 'clsx';
+import styles from './checkbox.module.css'
 
-function Checkbox () {
-  return <input type='checkbox'></input>
+function Checkbox ({name, id, className, defaultChecked, onChange}) {
+  return (
+  <input 
+  type='checkbox' 
+  name={name} 
+  id={id} 
+  className={cx(styles['checkbox'], className)} 
+  defaultChecked={defaultChecked}
+  onChange={onChange}></input>
+  )
 }
 
 export { Checkbox };
